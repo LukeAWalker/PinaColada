9db1e3a767c41f522d85d0f0a954925e2f10adc5
LukeAWalker <lukeawalker@hotmail.co.uk>
2016-04-23 14:21:11 +0100

    Added a main program and template for future files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*******************************************************************************
+
+Title:      main.cpp
+Author:     Luke
+Date:       23/04/2016
+
+Description:
+
+  Main file for JPL's new game; this file might contain the splash screen and
+main menu, for instance.
+
+*******************************************************************************/
---
+++ template.cpp
@@ -0,0 +1,12 @@
+/*******************************************************************************
+
+Title:      template.cpp
+Author:     Luke
+Date:       23/04/2016
+
+Description:
+
+  A template containing the header for each file. This should help organise the
+project and give uniform code.
+
+*******************************************************************************/
